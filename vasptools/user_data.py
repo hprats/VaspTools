227f153604cb7e24437c9c008d7acd1210c468f5
hprats <hpratsgarcia@gmail.com>
2025-01-04 15:25:10 +0100

    Updated scripts.

--- vasptools/user_data.py
+++ vasptools/user_data.py
@@ -3,4 +3,4 @@ Stores user-specific data and configuration for vasptools.
 For example, path to your local POTCAR library.
 """
 
-PP_PATH = '/Users/hprats/Documents/Codes/potpaw_PBE'
+PP_PATH = '/Users/hprats/Documents/Codes/potpaw_PBE_6.4_from_vsc5'
